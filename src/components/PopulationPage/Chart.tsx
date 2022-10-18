f4bbffac417effa3109cbbec9c9c9c457a4af00a
Hiro <hiro@HironoiMac.local>
2022-10-18 16:29:44 +0900

    feat: 縦軸、横軸の表記を直し、fetchしたデータをグラフ用に成形する関数を作成北海道の総人口推移を描画

--- src/components/PopulationPage/Chart.tsx
+++ src/components/PopulationPage/Chart.tsx
@@ -11,6 +11,8 @@ import {
   YAxis,
 } from "recharts";
 
+import makeGraphDataFromFetchData from "../../utils/makeGraphData";
+
 const PopulationChart = () => {
   const data = [
     {
@@ -109,17 +111,37 @@ const PopulationChart = () => {
   return (
     <ResponsiveContainer>
       <LineChart
-        data={data[0].data}
-        margin={{ top: 5, right: 60, left: 30, bottom: 5 }}
+        data={makeGraphDataFromFetchData(data)}
+        margin={{ top: 5, right: 60, left: 30, bottom: 90 }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="year" interval={0} angle={30} dx={20} />
-        <YAxis />
+        <XAxis
+          dataKey="year"
+          interval={0}
+          angle={45}
+          dx={20}
+          dy={20}
+          label={{
+            value: "調査年度（年）",
+            angle: 0,
+            position: "inside",
+            dy: 60,
+          }}
+        />
+        <YAxis
+          label={{
+            value: "人口(人)",
+            angle: 0,
+            position: "inside",
+            dy: -20,
+            dx: -30,
+          }}
+        />
         <Tooltip />
-        <Legend />
+        <Legend verticalAlign="top" />
         <Line
           type="monotone"
-          dataKey="value"
+          dataKey="北海道"
           stroke="#8884d8"
           activeDot={{ r: 8 }}
         />
