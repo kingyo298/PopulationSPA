f3f746e8feba1b0b16b14e2dff4f6ce7d2707ad4
Hiro <hiro@HironoiMac.local>
2022-10-19 16:22:45 +0900

    fix: rechartsのlegendをスタイリング responsive対応

--- src/components/PopulationPage/Chart/Chart.tsx
+++ src/components/PopulationPage/Chart/Chart.tsx
@@ -1,4 +1,4 @@
-import "@/assets/css/components/Rechart.css";
+import "@/assets/css/components/Recharts.css";
 
 import {
   CartesianGrid,
@@ -18,12 +18,17 @@ interface chartProps {
   populationData: populationDataPerYear<string>[];
   prefectures: Prefecture[];
 }
+const toolTipStyle = {
+  backgroundColor: "var(--primary-bg)",
+  height: "200px",
+  overflow: "scroll",
+};
 const PopulationChart = (props: chartProps) => {
   return (
     <ResponsiveContainer>
       <LineChart
         data={props.populationData}
-        margin={{ top: 5, right: 60, left: 30, bottom: 90 }}
+        margin={{ top: 5, right: 60, left: 30, bottom: 30 }}
       >
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis
@@ -48,8 +53,8 @@ const PopulationChart = (props: chartProps) => {
             dx: -30,
           }}
         />
-        <Tooltip />
-        <Legend verticalAlign="bottom" />
+        <Tooltip contentStyle={toolTipStyle} />
+        <Legend wrapperStyle={{ top: 0, left: 90 }} verticalAlign="top" />
         {props.populationData.length !== 0 &&
           Object.keys(props.populationData[0])
             .filter(key => key !== "year")
